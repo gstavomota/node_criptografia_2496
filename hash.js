1887cf87f3c5032a9e2e567f70bf6ad7cfe75b48
Gustavo Mota <gstavomota1@gmail.com>
2023-01-24 14:18:39 -0300

    hash

--- hash.js
+++ hash.js
@@ -1,2 +1,27 @@
 import { createHash } from 'crypto';
 
+function criaHash(senha){
+    return createHash('sha256').update(senha).digest("hex");
+}
+
+// console.log(criaHash("uma senha qq"));
+
+class Usuario {
+
+    constructor(nome, senha){
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+
+    autentica(nome, senha){
+        const hashSenha = criaHash(senha);
+        if(hashSenha == this.hash && nome === this.nome) console.log("Acesso permitido")
+        else console.log("Acesso negado");
+    }
+}
+
+const g = new Usuario("Gustavo", "1234");
+
+console.log(g);
+
+g.autentica("Gustavo", "1234");
