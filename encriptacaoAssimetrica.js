c4b8e21cb6312036d9db1da6bbb4b7829798e2d2
Gustavo Mota <gstavomota1@gmail.com>
2023-01-24 15:21:37 -0300

    encriptação Assimétrica

---
+++ encriptacaoAssimetrica.js
@@ -0,0 +1,35 @@
+import { generateKeyPairSync } from "crypto"; 
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+
+    publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem',
+    },
+    privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+    }
+})
+
+// console.log(privateKey);
+// console.log(publicKey);
+
+import { publicEncrypt, privateDecrypt } from "crypto";
+
+const dadosCriptografados = publicEncrypt(
+    publicKey,
+    Buffer.from("Mensagem secreta")
+)
+
+console.log(dadosCriptografados.toString('hex'));
+
+// =================== Transmissão ======================
+
+const dadosDecifrados = privateDecrypt(
+    privateKey,
+    dadosCriptografados
+)
+
+console.log(`Dados decifrados:${dadosDecifrados.toString('utf-8')}`);
